0c022bc12c2dfa4da8c7426ad13f77b10807f2cc
JavaCommons Technologies <javacommons@gmail.com>
2025-04-17 06:48:38 +0900

    2025.0417.0648

--- lib/src/command.dart
+++ lib/src/command.dart
@@ -1,11 +1,13 @@
 import 'package:misc/misc.dart' as misc;
 
+final _logger = misc.$globalLogger;
+
 int command(String $cmd, List<String> $cmdArgs) {
   String $commandLine = $cmd;
   for (int i = 0; i < $cmdArgs.length; i++) {
     $commandLine += ' "${$cmdArgs[i]}"';
   }
-  print('[Log] command(): ${$commandLine}');
+  _logger.i('command(): ${$commandLine}');
   return misc.wsystem($commandLine);
 }
 
