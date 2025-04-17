6f6b8af35c4b99ab1bc21bd47a666526e88600e9
JavaCommons Technologies <javacommons@gmail.com>
2025-04-17 20:31:28 +0900

    2025.0417.2031

--- test/exec_test.dart
+++ test/exec_test.dart
@@ -1,16 +1,16 @@
-import 'package:misc/misc.dart' as misc;
+//import 'package:misc/misc.dart' as misc;
 
 void main() async
 {
-  try {
-    misc.dump('hello');
-    final $api = misc.NativeJsonApi('PROGRAM.dll');
-    final $result = $api.call$('add2', 11, 22);
-    misc.dump($result);
-    final $result2 = $api.call$('add2', 11, 22, 33);
-    misc.dump($result2);
-  } catch ($e, $stacktrace) {
-    misc.dump($e, 'Exception');
-    misc.dump($stacktrace, 'Stacktrace');
-  }
+  // try {
+  //   misc.dump('hello');
+  //   final $api = misc.NativeJsonApi('PROGRAM.dll');
+  //   final $result = $api.call$('add2', 11, 22);
+  //   misc.dump($result);
+  //   final $result2 = $api.call$('add2', 11, 22, 33);
+  //   misc.dump($result2);
+  // } catch ($e, $stacktrace) {
+  //   misc.dump($e, 'Exception');
+  //   misc.dump($stacktrace, 'Stacktrace');
+  // }
 }
