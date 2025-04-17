8b03add107328779ee977eac11cb92725b54c27c
JavaCommons Technologies <javacommons@gmail.com>
2025-04-18 02:22:56 +0900

    2025.0418.0222

--- lib/misc.dart
+++ lib/misc.dart
@@ -4,7 +4,7 @@ export 'src/wsystem.dart';
 export 'src/command.dart';
 //export 'src/echo.dart';
 //export 'src/filesystem.dart';
-export 'src/global_logger.dart';
+//export 'src/global_logger.dart';
 export 'src/is_in_debug_mode.dart';
 //export 'src/native_json_api.dart';
 //export 'src/dynamic_function.dart';
